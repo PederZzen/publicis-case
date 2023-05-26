997bca0c10a8698233f541f013e4cd9c5c484451
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2023-05-26 10:43:31 +0200

    listing all data

--- src/pages/campaigns/index.tsx
+++ src/pages/campaigns/index.tsx
@@ -1,5 +1,67 @@
+import { useEffect, useState } from "react";
+import { ApiClient, Campaign } from "../../api-client";
+import Loader from "../../components/loader";
+
 const Campaigns = () => {
-  return <div></div>;
+  const [campaigns, setCampaigns] = useState<Campaign[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCampaigns = async () => {
+      try {
+        const apiClient = new ApiClient();
+        const campaignData = await apiClient.requestCampaigns();
+        setCampaigns(campaignData);
+        setIsLoading(false);
+        console.log(campaignData);
+      } catch (err) {
+        console.error(err);
+        setIsLoading(false);
+      }
+    };
+    fetchCampaigns();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (!campaigns) {
+    return <p>No data...</p>;
+  }
+
+  return (
+    <div>
+      <h1>Campaigns</h1>
+      <div>
+        {campaigns.map((campaign, idx) => {
+          return (
+            <div key={idx}>
+              <div>
+                <p>{campaign.name}</p>
+              </div>
+              <div>
+                <p>{campaign.client.name}</p>
+              </div>
+              <div>
+                <p>{campaign.startDate}</p>
+              </div>
+              <div>
+                <p>{campaign.endDate}</p>
+              </div>
+              <div>
+                <p>{campaign.campaignManager.name}</p>
+                <p>{campaign.campaignManager.email}</p>
+              </div>
+              <div>
+                <p>{campaign.budget}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Campaigns;
--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react";
+import { ApiClient, Client } from "../../api-client";
+import Loader from "../../components/loader";
+
 const Clients = () => {
-  return <div></div>;
+  const [clients, setClients] = useState<Client[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      try {
+        const apiClient = new ApiClient();
+        const clientData = await apiClient.requestClients();
+        setClients(clientData);
+        setIsLoading(false);
+        console.log(clientData);
+      } catch (err) {
+        console.error(err);
+        setIsLoading(false);
+      }
+    };
+    fetchClients();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (!clients) {
+    return <p>No data...</p>;
+  }
+
+  return (
+    <div>
+      <h1>Clients</h1>
+      <div>
+        {clients.map((client, idx) => {
+          return (
+            <div key={idx}>
+              <p>{client.name}</p>
+              <div>
+                <p>{client.defaultCampaignManager.name}</p>
+                <p>{client.defaultCampaignManager.email}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Clients;
